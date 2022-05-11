15525ab0238531b3651e6ac005f1e958020e611f
danielolson <dulcicannon@gmail.com>
2022-05-11 15:44:17 -0600

    Added a new tool to output total size of posterior matrices pre compression

---
+++ getpostsize.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print("usage: getpostsize file1 file2")
+    exit(0)
+
+file1 = sys.argv[1]
+file2 = sys.argv[2]
+
+file1 = open(file1)
+file2 = open(file2)
+
+
+lines1 = file1.readlines()
+lines2 = file2.readlines()
+
+if len(lines1) != len(lines2):
+    print("unequal number of lines!")
+    exit(-1)
+
+total_bytes = 0
+for i in range(len(lines1)):
+    if i % 2 == 0:
+        continue
+    total_bytes += len(lines[i]) * (len(lines[i]) + 5)
+
+print(total_bytes)
+
+file1.close()
+file2.close()
+
