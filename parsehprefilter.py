57df95845ab2312e5c0419360b5b7fb62bd1b5e2
danielolson <dulcicannon@gmail.com>
2022-05-24 14:18:18 -0600

    Completed hmmer prefilter parser

--- parsehprefilter.py
+++ parsehprefilter.py
@@ -23,7 +23,37 @@ with open(input_file, 'r') as file:
             if m:
                 start = m.start()
                 passed = int(line[start:line.find(" ", start)])
-                print(line, passed)
+                passed_MSV += passed
 
+        if line.startswith("Passed MSV filter:"):
+            m = re.search(r"\d", line)
+            if m:
+                start = m.start()
+                passed = int(line[start:line.find(" ", start)])
+                passed_MSV += passed
 
+        if line.startswith("Passed bias filter:"):
+            m = re.search(r"\d", line)
+            if m:
+                start = m.start()
+                passed = int(line[start:line.find(" ", start)])
+                passed_bias += passed
 
+        if line.startswith("Passed Vit filter:"):
+            m = re.search(r"\d", line)
+            if m:
+                start = m.start()
+                passed = int(line[start:line.find(" ", start)])
+                passed_viterbi += passed
+
+        if line.startswith("Passed Fwd filter:"):
+            m = re.search(r"\d", line)
+            if m:
+                start = m.start()
+                passed = int(line[start:line.find(" ", start)])
+                passed_forward += passed
+
+print(passed_MSV, "Passed MSV")
+print(passed_bias, "Passed bias")
+print(passed_viterbi, "Passed viterbi")
+print(passed_forward, "Passed forward")
